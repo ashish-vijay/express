62f0bc24d4b89e319e541018178fcf57a6d3a281
zueore <zueore@gmail.com>
2017-05-13 20:07:00 +0530

    Added log

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.url}`;
+  fs.appendFile('server.log', log + '\n');
+});
 
 app.listen(port, () => {
   console.log('Server is running on port 3000');
